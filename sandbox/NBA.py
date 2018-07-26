e385d1d1c1d5b82a455df19be593308568044371
dsananez <dsananez@gmail.com>
2018-07-26 09:56:20 -0300

    NBA v 1.3

--- sandbox/NBA.py
+++ sandbox/NBA.py
@@ -7,7 +7,6 @@ Created on Thu Jul  5 16:10:33 2018
 
 import pandas as pd
 import numpy as np
-import random
 import source as fp
 
 class ofertas():
@@ -38,6 +37,66 @@ class costos_class():
 		self.costos_campania = list(costos_campania_1)
 
 
+def fold(l, operador, neutro):
+  if l == []:
+    return(neutro)
+  e = l.pop(0)
+  return(operador(e, fold(l,operador,neutro)))
+def producto(x,y):
+  return(x*y)
+
+
+def q_esperado_sin_oferta(clientes,meses):
+    q_esperado = {}
+    for k in range(0,meses):
+        q_esperado[k] = 0
+        for i in range(0,len(clientes)):
+            cuenta = fold(list(1-clientes[i].cliente.prob_churn[0:k]),producto,1)
+            q_esperado[k] = q_esperado[k] + cuenta
+    return q_esperado
+
+def fact_esperado_sin_oferta(clientes,meses):
+    fact_esperado = {}
+    for k in range(0,meses):
+        fact_esperado[k] = 0
+        for i in range(0,len(clientes)):
+            cuenta = fold(list((1-clientes[i].cliente.prob_churn[0:k])),producto,1)
+            cuenta = cuenta*clientes[i].cliente.facturacion[k]
+            fact_esperado[k] = fact_esperado[k] + cuenta
+        if(k>=1):
+            fact_esperado[k] = fact_esperado[k] + fact_esperado[k-1]
+    return fact_esperado
+
+def q_esperado_oferta(churn_oferta,clientes,oferta,NBA_ani,umbral,meses):
+    q_esperado = {}
+    for k in range(0,meses):
+        q_esperado[k] = 0
+        for i in range(0,len(churn_oferta[oferta])):
+            if(NBA_ani.loc[oferta,'empresa_'+clientes[i].cuit+'_'+str(clientes[i].ani)]>=umbral):
+                cuenta = fold(list(1-churn_oferta[oferta].loc[:, churn_oferta[oferta].columns != 'ani'].loc[i][0:k]),producto,1)
+            else:
+                cuenta = fold(list((1-clientes[i].cliente.prob_churn[0:k])),producto,1)
+            q_esperado[k] = q_esperado[k] + cuenta
+    return q_esperado
+
+
+def fact_esperado_oferta(churn_oferta,fact_oferta,clientes,oferta,NBA_ani,umbral,meses):
+    q_esperado = {}
+    for k in range(0,meses):
+        q_esperado[k] = 0
+        for i in range(0,len(churn_oferta[oferta])):
+            if(NBA_ani.loc[oferta,'empresa_'+clientes[i].cuit+'_'+str(clientes[i].ani)]>=umbral):
+                cuenta = fold(list(1-churn_oferta[oferta].loc[:, churn_oferta[oferta].columns != 'ani'].loc[i][0:k]),producto,1)
+                cuenta = cuenta*fact_oferta[oferta].loc[:, churn_oferta[oferta].columns != 'ani'].loc[i,k]
+            elif(NBA_ani.loc[oferta,'empresa_'+clientes[i].cuit+'_'+str(clientes[i].ani)]<umbral):
+                cuenta = fold(list((1-clientes[i].cliente.prob_churn[0:k])),producto,1)
+                cuenta = cuenta*clientes[i].cliente.facturacion[k]
+            q_esperado[k] = q_esperado[k] + cuenta
+        if(k>=1):
+            q_esperado[k] = q_esperado[k] + q_esperado[k-1]
+    return q_esperado
+
+
 def calculo_cltv(dataOferta, dataSinOferta):
 
 ##Calculo del customer lifetime value
@@ -46,7 +105,7 @@ def calculo_cltv(dataOferta, dataSinOferta):
 	t2 = 1/dataOferta.cliente['prob_churn'].mean()
 
 
-	ltv_con = ((dataOferta.aceptacion *dataOferta.contactabilidad* ((dataOferta.cliente['facturacion']-dataSinOferta['costos'].values[i]-dataOferta.costos_sac-dataOferta.costos_camp).mean())*t2)+
+	ltv_con = ((dataOferta.aceptacion *dataOferta.contactabilidad* ((dataOferta.cliente['facturacion']-dataSinOferta['costos']-dataOferta.costos_sac-dataOferta.costos_camp).mean())*t2)+
     ((1-dataOferta.aceptacion)*((dataSinOferta['facturacion']-dataSinOferta['costos']-dataOferta.costos_camp).mean())*t1))
 
 
@@ -55,18 +114,18 @@ def calculo_cltv(dataOferta, dataSinOferta):
 	return ltv_con - ltv_sin
 
 
-def calculo_van_diff(dataOferta,dataSinOferta):
+def calculo_van_diff(dataOferta,dataSinOferta,meses):
 
     ##Calculo del valor actual neto con oferta
     van_con = 0
-    for i in range(0,round(len(dataOferta.cliente['facturacion']))):
+    for i in range(0,meses):
         van_con = van_con + (((dataOferta.aceptacion*dataOferta.contactabilidad*((1-dataOferta.cliente['prob_churn'].values[i]))*(dataOferta.cliente['facturacion'].values[i]-dataSinOferta['costos'].values[i]-dataOferta.costos_sac[i])+
-							(1-dataOferta.aceptacion)*(1-dataSinOferta['prob_churn'].values[i])*(dataSinOferta['facturacion'].values[i]-dataSinOferta['costos'].values[i]))-dataOferta.costos_camp[i])*(pow((1+dataOferta.tasa_interes.mean()),i)))
+							(1-dataOferta.aceptacion)*(1-dataSinOferta['prob_churn'].values[i])*(dataSinOferta['facturacion'].values[i]-dataSinOferta['costos'].values[i]))-dataOferta.costos_camp[i])/(pow((1+dataOferta.tasa_interes.mean()),i)))
 
     ##valor actual neto sin oferta
     van_sin = 0
-    for i in range(0,round(len(dataOferta.cliente['facturacion']))):
-        van_sin = van_sin + (dataSinOferta['facturacion'].values[i]-dataSinOferta['costos'].values[i])*((1-dataSinOferta['prob_churn'].values[i]))*(pow((1+dataOferta.tasa_interes.mean()),i))
+    for i in range(0,meses):
+        van_sin = van_sin + ((dataSinOferta['facturacion'].values[i]-dataSinOferta['costos'].values[i])*((1-dataSinOferta['prob_churn'].values[i])))/(pow((1+dataOferta.tasa_interes.mean()),i))
 
     #valor actual neto con oferta teniendo en cuenta la aceptacion de la oferta
     #van_oferta = (dataOferta.aceptacion)*van_con*dataOferta.contactabilidad + (1-dataOferta.aceptacion)*(van_sin-dataOferta.costos[0])
@@ -116,7 +175,7 @@ def oferta_por_cliente(NBA_ani,cuits):
     return scores_cuit_df
 
 
-def mejor_oferta(fact_oferta,churn_oferta,k,costos_of,aceptacion,tasa_interes,cliente,func_obj, contactabilidad):
+def mejor_oferta(fact_oferta,churn_oferta,k,costos_of,aceptacion,tasa_interes,cliente,func_obj, contactabilidad,meses):
 
     dataOferta = {}
     ##Ajusto variables de entrada para el calculo de los valores actuales cuando se hace una oferta
@@ -149,7 +208,7 @@ def mejor_oferta(fact_oferta,churn_oferta,k,costos_of,aceptacion,tasa_interes,cl
 
     for i in range(0,len(dataOferta)):
         if(func_obj == 'van'):
-            scores[dataOferta[i].oferta] = calculo_van_diff(dataOferta[i],dataSinOferta)
+            scores[dataOferta[i].oferta] = calculo_van_diff(dataOferta[i],dataSinOferta,meses)
         elif(func_obj == 'ltv'):
             scores[dataOferta[i].oferta] = calculo_cltv(dataOferta[i],dataSinOferta)
 
@@ -164,97 +223,89 @@ def mejor_oferta(fact_oferta,churn_oferta,k,costos_of,aceptacion,tasa_interes,cl
     #return ordenados
     return scores
 
-if __name__ == "__main__":
+def NBA(base_clientes,of,plataforma,func_obj,tipo_canal,meses,umbral):
 
     #BANCO DE PRUEBAS
 
-    ##DATOS CLIENTES NORMAL
-
-    d = {'ani': ['1','2','3','4','5','6','7','8','9','10','11','12'],
-	'cuit':['a','a','b','b','b','b','c','c','c','d','d','d'],
-    'fact': [220,250.3,230,500,420,125,600,623,121,597,875,210],
-	'costos': [100,50,20,200,100,50,100,100,40,150,200,20],
-    'churn_calibrated': [0.1,0.02,0.05,0.06,0.02,0.02,0.04,0.09,0.02,0.01,0.03,0.07],
-	'contactabilidad':[0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5],
-	'prob_upsell': [0.8,0.3,0.25,0.04,0.4,0.9,0.1,0.1,0.1,0.9,0.1,0.2],
-	'prob_downsell': [0.5,0.2,0.1,0.4,0.87,0.19,0.11,0.12,0.41,0.91,0.11,0.12],
-	'prob_cater': [0.25,0.72,0.91,0.54,0.29,0.59,0.71,0.62,0.41,0.11,0.781,0.412],
-	'prob_digitales': [0.5,0.172,0.1911,0.354,0.229,0.959,0.271,0.162,0.141,0.811,0.1,0.7412]}
-
-    data = pd.DataFrame(data=d)
+    #data = pd.read_csv('pruebas.csv',sep=';')
 
     clientes = {}
     df = pd.DataFrame()
 
     ##Datos del cliente
-    for i in range(0,len(data['ani'])):
-        ani = data['ani'].values[i]
-        cuit = data['cuit'].values[i]
-        id_ = 'empresa_'+data['cuit'].values[i]+'_'+data['ani'].values[i]
-        df['facturacion'] = pd.Series(data['fact'].values[i]*np.ones(24))
-        df['prob_churn'] = pd.Series(data['churn_calibrated'].values[i]*np.ones(24))
-        df['costos'] = pd.Series(data['costos'].values[i]*np.ones(24))
+    for i in range(0,len(base_clientes['ani'])):
+        ani = base_clientes['ani'].values[i]
+        cuit = base_clientes['cuit'].values[i]
+        id = 'empresa_'+base_clientes['cuit'].values[i]+'_'+str(base_clientes['ani'].values[i])
+        df['facturacion'] = pd.Series(base_clientes['fact'].values[i]*np.ones(24))
+        df['prob_churn'] = pd.Series(base_clientes['churn_calibrated'].values[i]*np.ones(24))
+        df['costos'] = pd.Series(base_clientes['costos'].values[i]*np.ones(24))
 
-        clientes[i] = cliente(ani,cuit,id_,df,data['contactabilidad'].values[i],data['prob_upsell'].values[i],data['prob_downsell'].values[i],data['prob_cater'].values[i],data['prob_digitales'].values[i])
+        clientes[i] = cliente(ani,cuit,id,df.copy(),base_clientes['contactabilidad'].values[i],base_clientes['prob_upsell'].values[i],base_clientes['prob_downsell'].values[i],base_clientes['prob_cater'].values[i],base_clientes['prob_digitales'].values[i])
+
+    ##Ofertas
+    #of = pd.read_csv('listado_campanas.txt', sep=",")
 
     #FACTURACION CUANDO SE APLICA LA OFERTA
-    fact_oferta = fp.fact_24(data)
+    fact_oferta = fp.fact_24(base_clientes,of)
 
     ##REEMPLAZAR POR EL CHURN CON OFERTA
-    churn_oferta = fp.churn_24(data)
+    churn_oferta = fp.churn_24(base_clientes,of)
 
-    for i in range(0,len(churn_oferta)):
-    	nombre = list(churn_oferta.keys())[i]
-    	churn_oferta[nombre][churn_oferta[nombre].columns[1:]] = (churn_oferta[nombre][churn_oferta[nombre].columns[1:]]/1000)*(random.random())
-
-    of = pd.read_csv('listado_campanas.txt', sep=",")
+    ##Plataforma
+    #plataforma = pd.read_csv('modelo_plataforma_cc.txt', sep=",")
 
     ##Aceptacion Ofertas
-    aceptacion_in = list(of['aceptacion_in'])
-    aceptacion_out = list(of['aceptacion_out'])
+    if(tipo_canal == 'IN'):
+        aceptacion = list(of['aceptacion_in'])
+    elif(tipo_canal =='OUT'):
+        aceptacion = list(of['aceptacion_out'])
 
     ##Costos
-
-    costos_in = costos_class(fp.costo_total(of)['cost_sac'],fp.costo_total(of)['cost_campana_in'])
-    costos_out = costos_class(fp.costo_total(of)['cost_sac'],fp.costo_total(of)['cost_campana_out'])
+    if(tipo_canal == 'IN'):
+        costos = costos_class(fp.costo_total(of,plataforma)['cost_sac'],fp.costo_total(of,plataforma)['cost_campana_in'])
+    elif(tipo_canal =='OUT'):
+        costos = costos_class(fp.costo_total(of,plataforma)['cost_sac'],fp.costo_total(of,plataforma)['cost_campana_out'])
 
     ##Tasa Interes
     tasa_interes = list(0.02*np.ones(24))
 
-    ##Funcion objetivo ('van' o 'ltv')
-    func_obj = 'van'
-
-    NBA_in = pd.DataFrame()
-    NBA_out = pd.DataFrame()
+    NBA_ = pd.DataFrame()
 
 
     for i in range(0,len(clientes)):
-        NBA_in[clientes[i].rs] = mejor_oferta(fact_oferta,churn_oferta,i,costos_in,aceptacion_in,tasa_interes,clientes[i],func_obj, data['contactabilidad'].values[i])
-        NBA_out[clientes[i].rs] = mejor_oferta(fact_oferta,churn_oferta,i,costos_out,aceptacion_out,tasa_interes,clientes[i],func_obj, data['contactabilidad'].values[i])
-
-    #print(NBA_in)
-    #print(NBA_out)
+        NBA_[clientes[i].rs] = mejor_oferta(fact_oferta,churn_oferta,i,costos,aceptacion,tasa_interes,clientes[i],func_obj, base_clientes['contactabilidad'].values[i],meses)
 
     ##OFERTA AGRUPADA POR CUIT
 
-    cuits = list(data['cuit'].unique())
-    oferta_cuit_in = oferta_por_cliente(NBA_in,cuits)
-    oferta_cuit_out = oferta_por_cliente(NBA_out,cuits)
+    cuits = list(base_clientes['cuit'].unique())
+    oferta_cuit = oferta_por_cliente(NBA_,cuits)
+
+    ##Retención clientes con y sin oferta
+    q = pd.DataFrame()
+    for i in range(0,len(of)):
+        q_of = q_esperado_oferta(churn_oferta,clientes,of['id_oferta'][i],NBA_,umbral,meses)
+        q[of['id_oferta'][i]] = pd.Series(q_of)
+
+    q['sin oferta'] = pd.Series(q_esperado_sin_oferta(clientes,meses))
+
+    ##Facturacion esperada con y sin oferta
+    fact = pd.DataFrame()
+    for i in range(0,len(of)):
+        f_of = fact_esperado_oferta(churn_oferta,fact_oferta,clientes,of['id_oferta'][i],NBA_,umbral,meses)
+        fact[of['id_oferta'][i]] = pd.Series(f_of)
+
+    fact['sin oferta'] = pd.Series(fact_esperado_sin_oferta(clientes,meses))
 
 
-    ##Para el IN
-    for i in range(0,len(oferta_cuit_in.columns)):
-        ordenados = oferta_cuit_in[oferta_cuit_in.columns.values[i]].sort_values(ascending=False)
-        print('Oferta principal para el cliente '+oferta_cuit_in.columns.values[i]+' en el IN es '+ordenados.index.values[0]+' con un score de '+str((ordenados.values[0])))
-        print('Oferta secundaria para el cliente '+oferta_cuit_in.columns.values[i]+' en el IN es '+ordenados.index.values[1]+' con un score de '+str((ordenados.values[1])))
-        print('-------------')
+    return oferta_cuit,NBA_,q,fact
 
+#if __name__ == "__main__":
+#
+#    base_clientes = pd.read_csv('pruebas.csv',sep=';')
+#    ofert = pd.read_csv('listado_campanas.txt', sep=",")
+#    plataforma = pd.read_csv('modelo_plataforma_cc.txt', sep=",")
+#
+#    [oferta_cuit,NBA_,q,fact] = NBA(base_clientes,ofert,plataforma,'van','IN',12)
 
-    ##Para el OUT
-    for i in range(0,len(oferta_cuit_out.columns)):
-        ordenados = oferta_cuit_out[oferta_cuit_out.columns.values[i]].sort_values(ascending=False)
-        print('Oferta principal para el cliente '+oferta_cuit_out.columns.values[i]+' en el OUT es '+ordenados.index.values[0]+' con un score de '+str((ordenados.values[0])))
-        print('Oferta secundaria para el cliente '+oferta_cuit_out.columns.values[i]+' en el OUT es '+ordenados.index.values[1]+' con un score de '+str((ordenados.values[1])))
-        print('-------------')
-
-                            	
+                   	
